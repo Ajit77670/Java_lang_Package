5a167f5ab620350278ef0dc9a381905eb70036ea
Ajit77670 <Ajit77670@gmail.com>
2023-01-01 00:50:55 +0530

    newlines

--- src/javalangpackg/Smaple_git_test.java
+++ src/javalangpackg/Smaple_git_test.java
@@ -11,6 +11,7 @@ public class Smaple_git_test {
         System.out.println("Hey Ajit");
         System.out.println("Happy new Year to you");
         System.out.println("Work hard and get all the Core IT SKILL IN 2023");
+        System.out.println("dlldldld");
         
     }
 }
