bc753752c715c3d96847e9b60baaed562b67b15a
Ajit77670 <Ajit77670@gmail.com>
2022-12-31 12:39:11 +0530

    new file added

---
+++ src/javalangpackg/Smaple_git_test.java
@@ -0,0 +1,12 @@
+
+package javalangpackg;
+
+
+public class Smaple_git_test {
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println("This is to ccheck the modify file get aaded to github");
+    }
+}
