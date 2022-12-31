511cf4ec946431143cbf8cc34a2e209de1bdfcf2
Ajit77670 <Ajit77670@gmail.com>
2023-01-01 00:36:23 +0530

    new class_file_added/code_updated

--- src/javalangpackg/Smaple_git_test.java
+++ src/javalangpackg/Smaple_git_test.java
@@ -8,5 +8,9 @@ public class Smaple_git_test {
     public static void main(String[] args) {
         
         System.out.println("This is to ccheck the modify file get aaded to github");
+        System.out.println("Hey Ajit");
+        System.out.println("Happy new Year to you");
+        System.out.println("Work hard and get all the Core IT SKILL IN 2023");
+        
     }
 }
